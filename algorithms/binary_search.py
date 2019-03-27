2f2f908ba07ed77967ba84adcec10b9bd67d55f1
Ugur Guney <ugurguney@gmail.com>
2019-03-27 00:53:54 -0400

    Init recursive binary search.

---
+++ algorithms/binary_search.py
@@ -0,0 +1,43 @@
+import logging
+
+# import random
+
+# n = 10
+# m = 20
+# all_nums = list(range(m))
+# random.shuffle(all_nums)
+# nums = all_nums[:n]
+# nums.sort()
+
+nums = [1, 2, 3, 5, 7, 8, 10, 14, 15, 17]
+logging.basicConfig(level=logging.DEBUG)
+logging.debug(nums)
+
+
+def binary_search(indexable, value):  # or sequence
+    def helper(low, high, value):
+        logging.debug(f"low: {low}, high: {high}")
+        if low > high:
+            return None
+
+        mid = (low + high) // 2  # low + (high - low) // 2
+        mid_val = indexable[mid]
+        logging.debug(f"mid: {mid}, mid_val: {mid_val}")
+        if mid_val == value:
+            logging.debug(f"found at {mid}")
+            return mid
+
+        if mid_val < value:
+            low = mid + 1
+        else:
+            high = mid - 1
+        return helper(low, high, value)
+
+    return helper(0, len(indexable) - 1, value)
+
+
+# print(binary_search([1, 2, 3, 5, 7, 8, 10, 14, 15, 17], 2))
+# print(binary_search([1, 2, 3, 5, 7, 8, 10, 14, 15, 17], 4))
+print(binary_search([1, 2, 3], 0))
+
+# TODO: https://github.com/vug/coding-moding/projects/5
