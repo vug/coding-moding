eabba0689b52e4772e1082ae6c98568587663f1d
Ugur Guney <ugurguney@gmail.com>
2019-04-07 19:50:33 -0400

    Implement Stack version of all_nearest_smaller_values.

--- algorithms/all_nearest_smaller_values.py
+++ algorithms/all_nearest_smaller_values.py
@@ -1,4 +1,8 @@
+from data_structures.stack import Stack
+
+
 def nsv(a, ix):
+    """Get nearest smaller (index, value) of a[ix] towards left."""
     iy = ix - 1
     while iy >= 0:
         if a[iy] < a[ix]:
@@ -8,6 +12,7 @@ def nsv(a, ix):
 
 
 def nsv_backward(a, ix):
+    """Get nearest smaller (index, value) of a[ix] towards right."""
     iy = ix + 1
     while iy < len(a):
         if a[iy] < a[ix]:
@@ -17,6 +22,7 @@ def nsv_backward(a, ix):
 
 
 def all_nearest_smaller_values_brute(a, is_forward=True, exceed_index=False):
+    """Brute-force computation of all nearest smaller values of array a."""
     r = []
     for ix, x in enumerate(a):
         if is_forward:
@@ -30,6 +36,7 @@ def all_nearest_smaller_values_brute(a, is_forward=True, exceed_index=False):
 
 
 def all_nearest_smaller_values(a):
+    """White board computation of all nearest smaller values of array a."""
     r, s = [], []
     for ix, x in enumerate(a):
         while s and s[-1][1] >= x:
@@ -42,13 +49,35 @@ def all_nearest_smaller_values(a):
     return r
 
 
-print(all_nearest_smaller_values_brute([2, 5, 7, 3, 3, 5, 4, 6, 1]))
-print(all_nearest_smaller_values([2, 5, 7, 3, 3, 5, 4, 6, 1]))
-print(all_nearest_smaller_values_brute([2, 5, 7, 3, 3, 5, 4, 6, 1], exceed_index=True))
-print(all_nearest_smaller_values_brute([2, 5, 7, 3, 3, 5, 4, 6, 1], is_forward=False))
-print(
-    all_nearest_smaller_values_brute(
-        [2, 5, 7, 3, 3, 5, 4, 6, 1], is_forward=False, exceed_index=True
+def all_nearest_smaller_values_v2(a):
+    """Compute all nearest smaller values of array a using a Stack."""
+    r = []
+    s = Stack()
+    for ix, x in enumerate(a):
+        while not s.is_empty() and s.peek()[1] >= x:
+            s.pop()
+        if s.is_empty():
+            r.append((None, None))  # (-1, None)
+        else:
+            r.append(s.peek())
+        s.push((ix, x))
+    return r
+
+
+# Some simple tests that does not use Stack version.
+if __name__ == "__main__":
+    print(all_nearest_smaller_values_brute([2, 5, 7, 3, 3, 5, 4, 6, 1]))
+    print(all_nearest_smaller_values([2, 5, 7, 3, 3, 5, 4, 6, 1]))
+    print(all_nearest_smaller_values_v2([2, 5, 7, 3, 3, 5, 4, 6, 1]))
+    print(
+        all_nearest_smaller_values_brute([2, 5, 7, 3, 3, 5, 4, 6, 1], exceed_index=True)
     )
-)
-print(all_nearest_smaller_values_brute([]))
+    print(
+        all_nearest_smaller_values_brute([2, 5, 7, 3, 3, 5, 4, 6, 1], is_forward=False)
+    )
+    print(
+        all_nearest_smaller_values_brute(
+            [2, 5, 7, 3, 3, 5, 4, 6, 1], is_forward=False, exceed_index=True
+        )
+    )
+    print(all_nearest_smaller_values_brute([]))
